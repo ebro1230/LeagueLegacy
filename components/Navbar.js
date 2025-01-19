218fc511e77d7a27dfd4532180fdfcfe1a0fd134
ebro1230 <ebro1230@gmail.com>
2025-01-19 11:46:28 +0100

    updating nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -34,9 +34,6 @@ function NavigationBar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav>
-            <Nav.Link href="/" className={koulen.className}>
-              Home
-            </Nav.Link>
             <Nav.Link href="/sport/football" className={koulen.className}>
               <Image
                 src={football}
@@ -78,21 +75,7 @@ function NavigationBar() {
               Baseball
             </Nav.Link>
           </Nav>
-          <Nav className="me-auto">
-            <NavDropdown title="Leagues" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="/sport/football">
-                Football
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/sport/hockey">Hockey</NavDropdown.Item>
-              <NavDropdown.Item href="/sport/basketball">
-                Basketball
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/sport/baseball">
-                Baseball
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
+          <Nav className="signin-div">
             {session && session.expires > Date.now() ? ( // If session exists
               <Nav.Link onClick={() => signOut()}>Sign Out</Nav.Link>
             ) : (
