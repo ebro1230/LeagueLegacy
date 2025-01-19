477bc0381ada61e95a34ee48e44605d0459cb94b
ebro1230 <ebro1230@gmail.com>
2025-01-19 16:54:58 +0100

    playing with cards

--- components/leagues-grid.js
+++ components/leagues-grid.js
@@ -22,7 +22,7 @@ export default function LeaguesGrid({ leagues, leagueType }) {
       <Container fluid>
         <Row>
           {leagues.map((league) => (
-            <Col md="12" lg="6" xl="4" key={`${league.leagueKeys} + Col`}>
+            <Col sm="12" lg="6" xl="4" key={`${league.leagueKeys} + Col`}>
               <Link
                 key={`${league.leagueKeys} + 1`}
                 href={`/sport/${leagueType}/league/${league.leagueKeys}`}
@@ -56,25 +56,74 @@ export default function LeaguesGrid({ leagues, leagueType }) {
                         height: "150px",
                         objectFit: "cover",
                         margin: "auto",
+                        color: "white",
                       }}
                       alt={`${league.leagueName}'s Logo`}
                     />
                   )}
                   <Card.Body>
-                    <p>League: {league.leagueName}</p>
-                    <p>Team Name: {league.teamName}</p>
-                    <p>
-                      {league.memberSince} -{" "}
-                      {league.memberUntil === currentYear
-                        ? "Present"
-                        : league.memberUntil}
-                    </p>
-                    <p>
-                      {league.wins} - {league.losses} - {league.ties}
-                    </p>
-                    <p>Win Percentage: {league.winPercentage}%</p>
-                    <p>Top Finish: {league.bestFinish}</p>
-                    <p>Bottom Finish: {league.worstFinish}</p>
+                    <Row>
+                      <p>League</p>
+                      <p> {league.leagueName}</p>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <p>Team Name</p> <p>{league.teamName}</p>
+                      </Col>
+                      <Col>
+                        <p>Member Years</p>
+                        <p>
+                          {league.memberSince} -{" "}
+                          {league.memberUntil === currentYear
+                            ? "Present"
+                            : league.memberUntil}
+                        </p>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <p>Record</p>
+                        <p>
+                          {league.wins} - {league.losses} - {league.ties}
+                        </p>
+                      </Col>
+                      <Col>
+                        <p>Win % </p>
+                        <p>{league.winPercentage}%</p>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <p>Top Finish</p>{" "}
+                        <p>
+                          {league.bestFinish}
+                          {league.bestFinish == 1
+                            ? "st Place"
+                            : league.bestFinish == 2
+                            ? "nd Place"
+                            : league.bestFinish == 3
+                            ? "rd Place"
+                            : league.bestFinish === "TBD"
+                            ? null
+                            : "th Place"}{" "}
+                        </p>
+                      </Col>
+                      <Col>
+                        <p>Bottom Finish</p>
+                        <p>
+                          {league.worstFinish}
+                          {league.worstFinish == 1
+                            ? "st Place"
+                            : league.worstFinish == 2
+                            ? "nd Place"
+                            : league.worstFinish == 3
+                            ? "rd Place"
+                            : league.worstFinish === "TBD"
+                            ? null
+                            : "th Place"}
+                        </p>
+                      </Col>
+                    </Row>
                   </Card.Body>
                 </Card>
               </Link>
