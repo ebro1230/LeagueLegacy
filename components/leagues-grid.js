9d0da7f18cdcff9b5c5f9418c8d3e61c11dfa76c
ebro1230 <ebro1230@gmail.com>
2025-01-19 15:57:03 +0100

    columns & rows

--- components/leagues-grid.js
+++ components/leagues-grid.js
@@ -3,6 +3,9 @@
 import Link from "next/link";
 import Image from "next/image";
 import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 export default function LeaguesGrid({ leagues, leagueType }) {
   const date = new Date();
@@ -16,63 +19,69 @@ export default function LeaguesGrid({ leagues, leagueType }) {
 
   return (
     <>
-      {leagues.map((league) => (
-        <Link
-          key={`${league.leagueKeys} + 1`}
-          href={`/sport/${leagueType}/league/${league.leagueKeys}`}
-        >
-          <Card
-            border="light"
-            style={{ backgroundColor: "transparent" }}
-            className="league-div"
-            key={league.leagueKeys}
-          >
-            {league.leagueLogo ? (
-              <Card.Img
-                variant="top"
-                src={league.leagueLogo}
-                className="rounded-circle"
-                style={{
-                  width: "150px",
-                  height: "150px",
-                  objectFit: "cover",
-                  margin: "auto",
-                }}
-                alt={`${league.leagueName}'s Logo`}
-              />
-            ) : (
-              <Card.Img
-                variant="top"
-                src="N/A"
-                className="rounded-circle"
-                style={{
-                  width: "150px",
-                  height: "150px",
-                  objectFit: "cover",
-                  margin: "auto",
-                }}
-                alt={`${league.leagueName}'s Logo`}
-              />
-            )}
-            <Card.Body>
-              <p>League: {league.leagueName}</p>
-              <p>Team Name: {league.teamName}</p>
-              <p>
-                {league.memberSince} -{" "}
-                {league.memberUntil === currentYear
-                  ? "Present"
-                  : league.memberUntil}
-              </p>
-              <p>
-                {league.wins} - {league.losses} - {league.ties}
-              </p>
-              <p>Win Percentage: {league.winPercentage}%</p>
-              <p>Top Finish: {league.bestFinish}</p>
-              <p>Bottom Finish: {league.worstFinish}</p>
-            </Card.Body>
-          </Card>
-        </Link>
-      ))}
+      <Container fluid>
+        <Row>
+          {leagues.map((league) => (
+            <Col md="12" lg="6" xl="4">
+              <Link
+                key={`${league.leagueKeys} + 1`}
+                href={`/sport/${leagueType}/league/${league.leagueKeys}`}
+              >
+                <Card
+                  border="light"
+                  style={{ backgroundColor: "transparent" }}
+                  className="league-div"
+                  key={league.leagueKeys}
+                >
+                  {league.leagueLogo ? (
+                    <Card.Img
+                      variant="top"
+                      src={league.leagueLogo}
+                      className="rounded-circle"
+                      style={{
+                        width: "150px",
+                        height: "150px",
+                        objectFit: "cover",
+                        margin: "auto",
+                      }}
+                      alt={`${league.leagueName}'s Logo`}
+                    />
+                  ) : (
+                    <Card.Img
+                      variant="top"
+                      src="N/A"
+                      className="rounded-circle"
+                      style={{
+                        width: "150px",
+                        height: "150px",
+                        objectFit: "cover",
+                        margin: "auto",
+                      }}
+                      alt={`${league.leagueName}'s Logo`}
+                    />
+                  )}
+                  <Card.Body>
+                    <p>League: {league.leagueName}</p>
+                    <p>Team Name: {league.teamName}</p>
+                    <p>
+                      {league.memberSince} -{" "}
+                      {league.memberUntil === currentYear
+                        ? "Present"
+                        : league.memberUntil}
+                    </p>
+                    <p>
+                      {league.wins} - {league.losses} - {league.ties}
+                    </p>
+                    <p>Win Percentage: {league.winPercentage}%</p>
+                    <p>Top Finish: {league.bestFinish}</p>
+                    <p>Bottom Finish: {league.worstFinish}</p>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </>
   );
 }
