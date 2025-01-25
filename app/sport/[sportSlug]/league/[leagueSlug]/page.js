ba920aefa70a7ffaba4b4be16a5b7f50d470fe08
ebro1230 <ebro1230@gmail.com>
2025-01-25 17:11:18 +0100

    fixing issues

--- app/sport/[sportSlug]/league/[leagueSlug]/page.js
+++ app/sport/[sportSlug]/league/[leagueSlug]/page.js
@@ -1,6 +1,4 @@
 import LeagueOverview from "@/components/league-overview4";
-import { Suspense } from "react";
-import LoadingIndicator from "@/components/loading-indicator";
 
 export default function LeaguePage({ params }) {
   const leagueType = params.sportSlug;
