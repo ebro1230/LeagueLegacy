4ffc66eb16de0b3609ecbecd97f74c729443c963
ebro1230 <ebro1230@gmail.com>
2025-01-25 18:57:44 +0100

    css

--- components/season-listgroup.js
+++ components/season-listgroup.js
@@ -3,19 +3,22 @@ import ListGroup from "react-bootstrap/ListGroup";
 
 export default function SeasonListGroup({ onSeasonSelect, leagueSeasons }) {
   return (
-    <div className="season-list-group-div">
-      <ListGroup onSelect={onSeasonSelect} horizontal variant="flush">
-        {leagueSeasons.map((season) => (
-          <ListGroup.Item
-            eventKey={JSON.stringify(season)}
-            key={season.key}
-            action
-            className="custom-list-group-item"
-          >
-            {season.year}
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
-    </div>
+    <ListGroup
+      onSelect={onSeasonSelect}
+      horizontal
+      variant="flush"
+      className="d-flex flex-wrap gap-3"
+    >
+      {leagueSeasons.map((season) => (
+        <ListGroup.Item
+          eventKey={JSON.stringify(season)}
+          key={season.key}
+          action
+          className="custom-list-group-item"
+        >
+          {season.year}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
   );
 }
