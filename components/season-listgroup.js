4bc2246128ff03c6617734b8a33210a76d98f089
ebro1230 <ebro1230@gmail.com>
2025-01-26 00:58:21 +0100

    adding divs to less than 10

--- components/season-listgroup.js
+++ components/season-listgroup.js
@@ -8,26 +8,30 @@ export default function SeasonListGroup({ onSeasonSelect, leagueSeasons }) {
       {leagueSeasons.length <= 10 ? (
         <>
           <ListGroup onSelect={onSeasonSelect} horizontal variant="flush">
-            <ListGroup.Item
-              eventKey={JSON.stringify(leagueSeasons[leagueSeasons.length - 1])}
-              key={leagueSeasons[leagueSeasons.length - 1].key}
-              action
-              className="custom-list-group-item"
-            >
-              {leagueSeasons[leagueSeasons.length - 1].year}
-            </ListGroup.Item>
-          </ListGroup>
-          <ListGroup onSelect={onSeasonSelect} horizontal variant="flush">
-            {leagueSeasons.slice(0, -1).map((season) => (
+            <div style={{ display: "flex", width: "fit-content" }}>
               <ListGroup.Item
-                eventKey={JSON.stringify(season)}
-                key={season.key}
+                eventKey={JSON.stringify(
+                  leagueSeasons[leagueSeasons.length - 1]
+                )}
+                key={leagueSeasons[leagueSeasons.length - 1].key}
                 action
                 className="custom-list-group-item"
               >
-                {season.year}
+                {leagueSeasons[leagueSeasons.length - 1].year}
               </ListGroup.Item>
-            ))}
+            </div>
+            <div style={{ display: "flex", width: "fit-content" }}>
+              {leagueSeasons.slice(0, -1).map((season) => (
+                <ListGroup.Item
+                  eventKey={JSON.stringify(season)}
+                  key={season.key}
+                  action
+                  className="custom-list-group-item"
+                >
+                  {season.year}
+                </ListGroup.Item>
+              ))}
+            </div>
           </ListGroup>
         </>
       ) : (
