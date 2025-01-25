a3989239ef0f6edb2132e8142ea3dcb3a65c260c
ebro1230 <ebro1230@gmail.com>
2025-01-25 23:39:20 +0100

    fixing sizing for season list

--- components/season-listgroup.js
+++ components/season-listgroup.js
@@ -3,19 +3,68 @@ import ListGroup from "react-bootstrap/ListGroup";
 
 export default function SeasonListGroup({ onSeasonSelect, leagueSeasons }) {
   return (
-    <div className="season-dropdown-div">
-      <ListGroup onSelect={onSeasonSelect} horizontal variant="flush">
-        {leagueSeasons.map((season) => (
-          <ListGroup.Item
-            eventKey={JSON.stringify(season)}
-            key={season.key}
-            action
-            className="custom-list-group-item"
-          >
-            {season.year}
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
+    <div className="season-listgroup-div">
+      <ListGroup.Item
+        eventKey={JSON.stringify(leagueSeasons[leagueSeasons.length].season)}
+        key={leagueSeasons[leagueSeasons.length].season.key}
+        action
+        className="custom-list-group-item"
+      >
+        {leagueSeasons[leagueSeasons.length].season.year}
+      </ListGroup.Item>
+      {leagueSeasons.length < 10 ? (
+        <ListGroup onSelect={onSeasonSelect} horizontal variant="flush">
+          {leagueSeasonsleagueSeasons.slice(0, -1).map((season) => (
+            <ListGroup.Item
+              eventKey={JSON.stringify(season)}
+              key={season.key}
+              action
+              className="custom-list-group-item"
+            >
+              {season.year}
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      ) : (
+        <div className="season-listgroup-div">
+          <ListGroup onSelect={onSeasonSelect} horizontal variant="flush">
+            <ListGroup.Item
+              eventKey={JSON.stringify(
+                leagueSeasons[leagueSeasons.length].season
+              )}
+              key={leagueSeasons[leagueSeasons.length].season.key}
+              action
+              className="custom-list-group-item"
+            >
+              {leagueSeasons[leagueSeasons.length].season.year}
+            </ListGroup.Item>
+          </ListGroup>
+          <ListGroup onSelect={onSeasonSelect} horizontal variant="flush">
+            {leagueSeasons.slice(0, 10).map((season) => (
+              <ListGroup.Item
+                eventKey={JSON.stringify(season)}
+                key={season.key}
+                action
+                className="custom-list-group-item"
+              >
+                {season.year}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+          <ListGroup onSelect={onSeasonSelect} horizontal variant="flush">
+            {leagueSeasons.slice(10, -1).map((season) => (
+              <ListGroup.Item
+                eventKey={JSON.stringify(season)}
+                key={season.key}
+                action
+                className="custom-list-group-item"
+              >
+                {season.year}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </div>
+      )}
     </div>
   );
 }
