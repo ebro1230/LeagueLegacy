20501aac45e61abe937411bc3ff9e1b5c44ca45b
ebro1230 <ebro1230@gmail.com>
2025-01-25 18:10:58 +0100

    imported listgroup

--- components/league-overview4.js
+++ components/league-overview4.js
@@ -14,6 +14,7 @@ import baseball from "@/assets/Fantasy-Baseball.png";
 
 import ManagerGrid from "./manager-grid3";
 import SeasonDropdown from "./season-dropdown";
+import SeasonListGroup from "./season-listgroup";
 import TeamDropdown from "./team-dropdown";
 import ManagerComparison from "./manager-comparison";
 import TrendsDropdown from "./trends-dropdown";
