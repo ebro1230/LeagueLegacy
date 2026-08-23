3f67ce12d222d974b225fe4d6932ca87787b5e54
ebro1230 <ebro1230@gmail.com>
2026-08-23 12:24:33 +0200

    fixing yahoo attribution

--- components/Footer.js
+++ components/Footer.js
@@ -19,15 +19,15 @@ function Footer() {
       variant="dark"
     >
       <Container>
-        <Navbar.Brand href="/" className={koulen.className}>
+        <Navbar.Brand style={{ fontSize: "1rem", color: "white" }}>
           Fantasy data provided by Yahoo Fantasy
         </Navbar.Brand>
         <Image
           src={`https://763445962456-brand-assets.s3.us-west-2.amazonaws.com/brandwebsite/s3fs-public/Yahoo_Fantasy.svg`}
-          height={20}
+          width={500}
           placeholder="blur"
-          //style={logoStyle}
-          alt={`Picture for fantasy football league`}
+          objectFit="contain"
+          alt={`Yahoo! Fantasy Logo`}
         />
       </Container>
     </Navbar>
